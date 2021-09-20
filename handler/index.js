2921a22640a6afd60068c2d61a159be0e03f8dfb
Benny Jou <a33068843@gmail.com>
2021-09-21 06:25:53 +0800

    fix: 用 config 讓 Handler 拿取頻道 ID

--- handler/index.js
+++ handler/index.js
@@ -1,8 +1,13 @@
+const config = require("../json/config.json");
 const { glob } = require("glob");
 const { promisify } = require("util");
 const globPromise = promisify(glob);
 
 module.exports = async (client) => {
+  // Events
+  const eventFiles = await globPromise(`${process.cwd()}/events/*.js`);
+  eventFiles.map((value) => require(value));
+
   // Commands
   const commandFiles = await globPromise(`${process.cwd()}/commands/**/*.js`);
   commandFiles.map((value) => {
@@ -11,29 +16,27 @@ module.exports = async (client) => {
     const directory = splitted[splitted.length - 2];
 
     if (file.name) {
-        const properties = { directory, ...file };
-        client.commands.set(file.name, properties);
+      const properties = { directory, ...file };
+      client.commands.set(file.name, properties);
     }
   });
-
-  // Events
-  const eventFiles = await globPromise(`${process.cwd()}/events/*.js`);
-  eventFiles.map((value) => require(value));
 
   // Slash Commands
   const slashCommands = await globPromise(`${process.cwd()}/SlashCommands/*/*.js`);
 
   const arrayOfSlashCommands = [];
+    slashCommands.map((value) => {
+      const file = require(value);
+      if (!file?.name) return;
+      client.slashCommands.set(file.name, file);
 
-  slashCommands.map((value) => {
-    const file = require(value);
-    if (!file?.name) return;
-
-    client.slashCommands.set(file.name, file);
-    arrayOfSlashCommands.push(file);
-  });
+      if (["MESSAGE", "USER"].includes(file.type)) delete file.description;
+      arrayOfSlashCommands.push(file);
+    });
 
   client.on('ready', async () => {
-    await client.commands.set(arrayOfSlashCommands);
+    await client.guilds.cache
+      .get(config.channelId)
+      .commands.set(arrayOfSlashCommands);
   })
 }
